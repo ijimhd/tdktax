949b2ca8c678c8748c0360542273ed7ec38ad2cc
ijimhd <karanikolosd@gmail.com>
2024-04-18 16:49:03 +0300

    Fixed the highlight of the current active page

--- js/activePage.js
+++ js/activePage.js
@@ -1,6 +1,5 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a.nav-link').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
-        link.classList.add('active');
+document.querySelectorAll('.nav-link').forEach (link =>{
+    if(link.href === window.location.href){
+        link.setAttribute('aria-current', 'page')
     }
 })
